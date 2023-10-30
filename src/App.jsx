6a260a7008b352a596b69b2af521e254bae99757
Md Adil Khan <mdadilakhtar8@gmail.com>
2023-10-30 22:43:39 +0530

    loginsingup page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+
+// import React, {lazy,Suspense} from 'react'
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider,LinearProgress } from '@mui/material';
+import theme from './customTheme/Theme';
+
+const Login = lazy(() => import('./components/Login'));
+const SignUp = lazy(() => import('./components/SignUp'));
+
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Suspense fallback={<LinearProgress  />}>
+          <Routes>
+            <Route path='/login' element={<Login/>} />
+            <Route path='/signup' element={<SignUp/>} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </ThemeProvider>
     </>
   )
 }
-
+ 
 export default App
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,234 @@
+
+import { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import {
+    Box,
+    Paper,
+    Typography,
+    TextField,
+    Button,
+    InputAdornment,
+    IconButton,
+} from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import LockIcon from "@mui/icons-material/Lock";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { LinearProgress } from '@mui/material';
+import {Link} from 'react-router-dom'
+
+
+
+const SignUp = () => {
+    const theme = useTheme();
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <>
+
+
+            <Grid>
+                <Paper
+                    elevation={12}
+                    sx={{
+                        height: "600px",
+                        width: "350px",
+                        margin: "70px auto",
+                        paddingTop: "60px",
+                        background: "#202124",
+                        border: `${theme.palette.secondary.main}`,
+                        boxShadow: "0px 0px 20px 0px #fbbd05",
+                        borderRadius: "16px",
+                        textAlign: "center",
+                    }}
+                >
+                    <Grid align="center">
+                        <Avatar sx={{ background: `${theme.palette.secondary.main}` }}>
+                            <LockIcon />
+                        </Avatar>
+                        <Typography
+                            variant="h2"
+                            sx={{
+                                margin: "20px 0px",
+                                color: `${theme.palette.primary.contrastText}`,
+                            }}
+                        >
+                            Sign Up
+                        </Typography>
+                    </Grid>
+                    <Grid align="center" sx={{ padding: "20px" }}>
+                        <Grid sx={{ marginBottom: "20px" }}>
+                            <TextField
+                                id="outlined-email-input"
+                                label="Email"
+                                placeholder="Email"
+                                fullWidth
+                                InputProps={{
+                                    sx: {
+                                        fontSize: "18px", // Change font size
+                                        color: `${theme.palette.secondary.main}`, // Change text color
+                                        "& .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color
+                                        },
+                                        "&:hover .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color on hover
+                                        },
+                                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color when focused
+                                        },
+                                        "& input::placeholder": {
+                                            color: `${theme.palette.secondary.main}`, // Change placeholder color
+                                        },
+                                    },
+                                }}
+                                InputLabelProps={{
+                                    sx: {
+                                        fontSize: '18px',
+                                        color: `${theme.palette.secondary.main}`, // Change label color
+                                        "&.Mui-focused": {
+                                            color: theme.palette.secondary.main, // Change label color on focus
+                                        },
+                                    },
+                                }}
+                            />
+                        </Grid>
+                        <Grid sx={{ marginBottom: "20px" }}>
+                            <TextField
+                                id="outlined-username-input"
+                                label="Username"
+                                placeholder="Username"
+                                fullWidth
+                                InputProps={{
+                                    sx: {
+                                        fontSize: "18px", // Change font size
+                                        color: `${theme.palette.secondary.main}`, // Change text color
+                                        "& .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color
+                                        },
+                                        "&:hover .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color on hover
+                                        },
+                                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color when focused
+                                        },
+                                        "& input::placeholder": {
+                                            color: `${theme.palette.secondary.main}`, // Change placeholder color
+                                        },
+                                    },
+                                }}
+                                InputLabelProps={{
+                                    sx: {
+                                        fontSize: '18px',
+                                        color: `${theme.palette.secondary.main}`, // Change label color
+                                        "&.Mui-focused": {
+                                            color: theme.palette.secondary.main, // Change label color on focus
+                                        },
+                                    },
+                                }}
+                            />
+                        </Grid>
+
+                        <Grid sx={{ marginBottom: "20px" }}>
+                            <TextField
+                                id="outlined-password-input"
+                                label="Password"
+                                type={showPassword ? 'text' : 'password'}
+                                autoComplete="current-password"
+                                fullWidth
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            {/* Password visibility toggle button */}
+                                            <Visibility
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                sx={{
+                                                    cursor: "pointer",
+                                                    color: theme.palette.secondary.main,
+                                                }}
+                                            />
+                                        </InputAdornment>
+                                    ),
+                                    sx: {
+
+                                        fontSize: "18px", // Change font size
+                                        color: `${theme.palette.secondary.main}`, // Change text color
+                                        "& .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color
+                                        },
+                                        "&:hover .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color on hover
+                                        },
+                                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                                            borderColor: `${theme.palette.secondary.main}`, // Change outline color when focused
+                                        },
+                                        "& input::placeholder": {
+                                            color: `${theme.palette.secondary.main}`, // Change placeholder color
+                                        },
+                                    },
+                                }}
+                                InputLabelProps={{
+                                    sx: {
+                                        fontSize: '18px',
+                                        color: `${theme.palette.secondary.main}`, // Change label color
+                                        "&.Mui-focused": {
+                                            color: theme.palette.secondary.main, // Change label color on focus
+                                        },
+                                    },
+                                }}
+                            />
+                        </Grid>
+                        <Grid>
+                            <Button
+                                disableRipple={true}
+                                sx={{
+
+                                    backgroundColor: `${theme.palette.secondary.main}`,
+                                    color: `${theme.palette.primary.main}`,
+                                    marginTop: "20px",
+                                    width: "25%",
+                                    borderRadius: "16px",
+                                    "&:hover": {
+                                        backgroundColor: `${theme.palette.primary.main}`, // No change in background color on hover
+                                        color: `${theme.palette.secondary.main}`, // No change in text color on hover
+                                        boxShadow: "0px 0px 20px 0px #fbbd05",
+                                    },
+                                }}
+                            >
+                                Sign Up
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Link to='/login'>
+                    <Button
+                        disableRipple={true}
+                        sx={{
+                            textTransform: 'none',
+                            color: `${theme.palette.primary.contrastText}`,
+                            "&:hover": {
+                                color: `${theme.palette.secondary.main}`,
+                            },
+                        }}
+                    >
+                        Already a User?
+                    </Button>
+                    </Link>
+                    
+                </Paper>
+            </Grid>         
+        </>
+
+    )
+}
+
+
+
+export default SignUp;
+
+
